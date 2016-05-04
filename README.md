3f8ee552f53f8d9c93b366dab49b36f2ddd96abd
Phil Cohen <phillip@phillip.io>
2016-05-04 11:18:57 -0700

    v1

---
+++ README.md
@@ -0,0 +1,14 @@
+# IsItChristmasOSX (beta)
+
+A native desktop app that brings the power of http://isitchristmas.com to your desktop.
+
+![preview](http://i.imgur.com/zvSYKb3.png)
+
+## Features
+
+- Feature completeness with http://isitchristmas.com (coming Q4 2016)
+
+## Installation
+
+- Check out the project
+- Run it in Xcode
---
+++ isitchristmas/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  isitchristmas
+//
+//  Created by phil on 3/23/16.
+//  Copyright © 2016 phillco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength);
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        let string = NSMutableAttributedString(string: "NO");
+        string.beginEditing();
+        string.addAttribute(NSFontAttributeName, value: NSFont.boldSystemFontOfSize(14), range: NSMakeRange(0, 2));
+        string.endEditing();
+        
+        statusItem.attributedTitle = string;
+        statusItem.toolTip = "It is not currently Christmas.";
+        statusItem.action = #selector(statusItemClicked);
+    }
+    
+    func statusItemClicked() {
+        let menu = NSMenu();
+        menu.addItem(NSMenuItem(title: "It is not currently Christmas.", action: nil, keyEquivalent: ""));
+        menu.addItem(NSMenuItem.separatorItem());
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: ""));
+        statusItem.popUpStatusItemMenu(menu);
+    }
+    
+    func quit() {
+        NSApplication.sharedApplication().terminate(self);
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
